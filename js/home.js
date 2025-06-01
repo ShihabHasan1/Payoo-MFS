ecec821874d8998024d7238509793f9a4438b496
ShihabHasan1 <shihabhasan685@gmail.com>
2025-06-01 22:31:42 +0600

    get the current amount and pin number

--- js/home.js
+++ js/home.js
@@ -1,1 +1,8 @@
-console.log('home Added')
+document.getElementById('input-add-money')
+    .addEventListener('click' , function(event){
+        event.preventDefault();
+        const inputAddMoney = document.getElementById('input-add-amount').value;
+        console.log(inputAddMoney);
+        const inputPinNumber = document.getElementById('input-pin-number').value;
+        console.log(inputPinNumber);
+    })
