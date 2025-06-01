9e372fd2f569cc1f56da8136a1db7b88f944796e
ShihabHasan1 <shihabhasan685@gmail.com>
2025-06-01 23:06:29 +0600

    check valid pin number if this pin is valid then add money to current balance and set the new balance

--- js/home.js
+++ js/home.js
@@ -5,4 +5,19 @@ document.getElementById('input-add-money')
         console.log(inputAddMoney);
         const inputPinNumber = document.getElementById('input-pin-number').value;
         console.log(inputPinNumber);
+
+        if(inputPinNumber === '1234'){
+            const balance = document.getElementById('current-balance').innerText;
+            console.log(balance);
+
+            const addMoney = parseFloat(inputAddMoney);
+            const currentBalance = parseFloat(balance);
+            const newBalance = addMoney + currentBalance;
+            console.log(newBalance);
+
+            document.getElementById('current-balance').innerText = newBalance;
+        }
+        else{
+            alert('Fail to add money ! please try again letter');
+        }
     })
